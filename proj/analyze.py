2f1e8cad8c7111eb88ab56a220be1b9462221e0b
Nigel Nindo <nigelnindo@gmail.com>
2016-04-29 22:02:29 +0300

    First working implementation of naive bayes classification

--- proj/analyze.py
+++ proj/analyze.py
@@ -24,6 +24,43 @@ issue_recognition_classifier = None
 domain_feature_list = []
 issue_feature_list = []
 
+
+def cleanTweet(tweet_text):
+    # Convert to lower case
+    tweet_text = tweet_text.lower()
+    # Convert www.* or https?://* to URL
+    tweet_text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL',tweet_text)
+    # Convert @username to AT_USER
+    tweet_text = re.sub('@[^\s]+','AT_USER',tweet_text)
+    #Remove additional white spaces
+    tweet_text = re.sub('[\s]+', ' ', tweet_text)
+    #Replace #word with word
+    tweet_text = re.sub(r'#([^\s]+)', r'\1', tweet_text)
+    #trim
+    tweet_text = tweet_text.strip('\'"')
+    print 'tweet pre-processed:'
+    print tweet_text
+    return tweet_text
+
+# look for 2 or more repetitions of character and replace with the character itself
+def replaceTwoOrMore(word):
+    pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
+    return pattern.sub(r"\1\1", word)
+
+def getFeaturevector(tweet_text):
+    feature_vector = []
+    words = tweet_text.split()
+    for word in words:
+        word = replaceTwoOrMore(word=word)
+        # strip out punctuation
+        word = word.strip('\'"?,.')
+        # check if the word stats with an alphabet
+        # TODO: find out if this works for Swahili
+        # val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*$", word)
+        # TODO: Add list of stop words that don't add meaning to the feature vector
+        feature_vector.append(word.lower())
+    return feature_vector
+
 # use this to extract features of a domain
 def domainExtractFeatures(feature_vector):
     tweet_words = set(feature_vector)  # remove duplicate vectors from tweet
@@ -38,8 +75,8 @@ def domainExtractFeatures(feature_vector):
     return features
 
 def getDomainRecognitionList():
-    electricity_trainer_csv = open('proj/domain_keywords.csv', "rb")
-    reader = csv.reader(electricity_trainer_csv)
+    domain_keywords_csv = open('proj/domain_keywords.csv', "rb")
+    reader = csv.reader(domain_keywords_csv)
 
     domain_list = [] # tuple containing a word and its domain
 
@@ -63,8 +100,17 @@ def getDomainRecognitionList():
 def getDommainRecognitionTrainingSet():
     # TODO: Read data from domain_training_set.csv, pass it to you extractor & use that information and results to train your classifier
 
+    domain_training_set_csv = open('proj/domain_training_set.csv',"rb")
+    reader = csv.reader(domain_training_set_csv)
+
     training_set = []  # training set is a list of tuples (features, 'result')
 
+    for row in reader:
+        cleaned_tweet_text = cleanTweet(tweet_text=row[0])
+        feature_vector = getFeaturevector(tweet_text=cleaned_tweet_text)
+        training_set.append((domainExtractFeatures(feature_vector=feature_vector), row[1]))  # result of feature extraction, & expected result
+
+    return training_set
 
 # to train the naive bayes algorithm, you need a tuple:
     # (dictionary, label)
@@ -72,7 +118,7 @@ def getDommainRecognitionTrainingSet():
 #initialize lists and classifiers in memory
 
 domain_feature_list = getDomainRecognitionList()
-domain_recongition_classifier = nltk.NaiveBayesClassifier.train()
+domain_recongition_classifier = nltk.NaiveBayesClassifier.train(getDommainRecognitionTrainingSet())
 
 
 def getIssueRecognitionList():
@@ -91,41 +137,6 @@ def analyzeTweet(tweet):
     print 'Showing classifier output:'
     print domain_recongition_classifier.classify(domainExtractFeatures(feature_vector=feature_vector))
 
-def cleanTweet(tweet_text):
-    # Convert to lower case
-    tweet_text = tweet_text.lower()
-    # Convert www.* or https?://* to URL
-    tweet_text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL',tweet_text)
-    # Convert @username to AT_USER
-    tweet_text = re.sub('@[^\s]+','AT_USER',tweet_text)
-    #Remove additional white spaces
-    tweet_text = re.sub('[\s]+', ' ', tweet_text)
-    #Replace #word with word
-    tweet_text = re.sub(r'#([^\s]+)', r'\1', tweet_text)
-    #trim
-    tweet_text = tweet_text.strip('\'"')
-    print 'tweet pre-processed:'
-    print tweet_text
-    return tweet_text
-
-    # look for 2 or more repetitions of character and replace with the character itself
-def replaceTwoOrMore(word):
-    pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
-    return pattern.sub(r"\1\1", word)
-
-def getFeaturevector(tweet_text):
-    feature_vector = []
-    words = tweet_text.split()
-    for word in words:
-        word = replaceTwoOrMore(word=word)
-        # strip out punctuation
-        word = word.strip('\'"?,.')
-        # check if the word stats with an alphabet
-        # TODO: find out if this works for Swahili
-        # val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*$", word)
-        # TODO: Add list of stop words that don't add meaning to the feature vector
-        feature_vector.append(word.lower())
-    return feature_vector
 
     # finds out whether this is a water or electricty issue
 def issueCategorization():
