b3ab57182b39d586128a157633518f335d7236ce
Nigel Nindo <nigelnindo@gmail.com>
2016-04-29 12:57:50 +0300

    Function declarations for application pipeline process

--- proj/analyze.py
+++ proj/analyze.py
@@ -1,11 +1,28 @@
 from __future__ import absolute_import
 
+"""
+Analyze the tweet and try to figure out what the user means
+"""
+
 from proj.celery import app
 
 import pycassa
 
-
 @app.task
 def analyze_tweet(tweet):
-    print 'about to tokenize tweet'
-    print tweet
+    print 'about to analyze tweet'
+
+def clean_tweets(tweet):
+    pass
+
+def issue_categorization():
+    pass
+
+def issue_time_categorization():
+    pass
+
+def issue_entity_recognition():
+    pass
+
+def organization_matcher():
+    pass
--- proj/tasks.py
+++ proj/tasks.py
@@ -6,9 +6,9 @@ import time
 from celery import Task
 
 from proj.celery import app
-from celery.decorators import periodic_task
+from celery.decorators import analyze_tweet
 
-from proj.analyze import analyze_tweet
+from proj.analyze import issue_categorization
 
 import pycassa
 
@@ -22,16 +22,17 @@ import pycassa
 A Cassandra table created with compact storage can only have one column that is not part of the primary key.
 """
 
-
 @app.task
 def save_raw_tweet_to_cassandra(tweet):
     pool = pycassa.ConnectionPool('tweets')
     cf = pycassa.ColumnFamily(pool, 'rawtweets')
     cf.insert(tweet['id_str'], {'tweet_text': tweet['text'], 'tweet_timestamp': int(tweet['timestamp_ms'][:-3]), 'user_id': tweet['user']['id_str'], 'user_name': tweet['user']['name'], 'user_screen_name': tweet['user']['screen_name'], 'user_created_at': tweet['user']['created_at']})
     # after successfully storing the tweet, send it for analysis
-    analyze_tweet.apply_async((tweet,))
+    issue_categorization.apply_async((tweet,))
 
 
+# we won't be recieving raw tweets from Cassandra anymore
+# it's quite likely we wont need this periodic task
 @periodic_task(run_every=timedelta(seconds= 30))
 def read_raw_tweets_from_cassandra():
     pass
